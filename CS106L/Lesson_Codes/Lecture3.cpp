bfc78746136948cb79a5edfd633960fdc441327d
warrior <1986411713@qq.com>
2024-04-30 17:01:35 +0800

    add a book for cmake

---
+++ CS106L/Lesson_Codes/Lecture3.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Lenovo on 2024/4/30.
+//
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
+
+
+/*
+ * 结构绑定(c++17)：auto [ , , ] = struct or tuple...
+ * struct能用于作为函数返回值，以实现多个数据的返回
+ * uniform initialization{}
+ * */
