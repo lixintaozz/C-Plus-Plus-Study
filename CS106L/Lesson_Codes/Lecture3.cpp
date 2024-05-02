140301e8671c88c8d1625f0bb938ca22fa2bfe3a
warrior <1986411713@qq.com>
2024-05-02 11:47:17 +0800

    add Lecture3.cpp

--- CS106L/Lesson_Codes/Lecture3.cpp
+++ CS106L/Lesson_Codes/Lecture3.cpp
@@ -4,11 +4,18 @@
 
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
 int main(){
-
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+/*  //使用[]来访问vector元素时，不会检查下标访问是否越界，原因是为了提高程序的运行效率，节省时间
+    cout << vec[15] << endl;
+    //使用at()方法访问vector元素时，会检查下标访问是否越界，如果越界，编译器会报错
+    cout << vec.at(15) << endl;*/
     return 0;
 }
 
@@ -18,3 +25,7 @@ int main(){
  * struct能用于作为函数返回值，以实现多个数据的返回
  * uniform initialization{}
  * */
+
+/*
+ * vector访问操作更快,deque(双端队列)适用于push_front()的情况
+ */
