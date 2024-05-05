ccbbf17b4f7cbf3aa84b0ab8a67a5f1998d54c69
warrior <1986411713@qq.com>
2024-05-05 14:49:25 +0800

    add Lecture5.cpp

--- CS106L/Lesson_Codes/Lecture4.cpp
+++ CS106L/Lesson_Codes/Lecture4.cpp
@@ -26,7 +26,8 @@ int main()
  * 容器适配器：stack, queue   它们本身不是容器，只是限制了某些功能的顺序容器
  *
  * 关联容器：set, map, unordered set, unordered map   //[key]不检查key是否存在；get(key)检查key是否存在，不存在的话会throw an error
- * 使用自定义的类对象作为map或set的typename时，需要重载 < 运算符，因为map和set是有序的
+ *         使用自定义的类对象作为map或set的typename时，需要重载 < 运算符，因为map和set是有序的
+ *         count()方法返回关键字在set/map中出现的次数(只能为0或1)，但它的速度没有find()方法快，所以更推荐使用find()；在c++20中可用contains()来取代
  * */
 /*
  * 运算符重载：
---
+++ CS106L/Lesson_Codes/Lecture5.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Lenovo on 2024/5/5.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main(){
+    std::vector<int> vec{5, 4, 3, 2, 1};
+    for (auto i:vec){
+        std::cout << i << ' ';
+    }
+    std::cout << std::endl;
+
+    //find返回一个iterator，如果能找到则返回指向第一个val的iterator，否则返回end()
+    auto it = find(vec.begin(), vec.end(), 5);
+    if (it == vec.end()) std::cout << "Cannot find val";
+    else std::cout << "Find " << *it << " successfully!";
+    std::cout << std::endl;
+    return 0;
+}
+
+
+/*
+ * 几种创建std::pair<string, int>的方式
+ * - std::pair<string, int> p{"Phone number", 6532565};  //uniform initialization
+ * - std::make_pair("Phone number", 6532565);
+ * - {"Phone number", 6532565};
+ * */
+
+/*
+ * 两个可用于迭代器遍历的方法，具体的使用方法待后续使用时再查询
+ * lower_bound()
+ * upper_bound()
+ * */
+
+/*五种迭代器的类型：
+ * input iterator（顺序，只读，只能被单次解引用）
+ *    ===> forward iterator（同时包含input iter和output iter的功能，能够多次解引用）
+ *    ===> bidirectional iterator（能--的forward iter）===> random iterator（能jmup的bidirectional iter）
+ * output iterator（顺序，只写，只能被单次解引用）
+ * */
+
+
