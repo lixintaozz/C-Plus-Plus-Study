9427ce7f3b48a4abda2fdf17d9a3c10f867099f1
warrior <1986411713@qq.com>
2024-05-14 09:26:53 +0800

    测试back_inserter的用法

--- CS106L/Lesson_Codes/Lecture9.cpp
+++ CS106L/Lesson_Codes/Lecture9.cpp
@@ -11,11 +11,11 @@ using namespace std;
 int main(int argc, char* argv[]){
     string str{"avcTEgehGHJJlkdj"};
     cout << "Before process: " << str << endl;
-    string res = "";
+    string res{""};
     auto op = [](char& ch){return (char)(ch + 1);};
     //transform对容器中的元素进行转换
     //Todo:为什么unary predicate使用std::tolower()不行？？？
-    transform(str.begin(),str.end(), str.begin(), op);
-    cout << "After process: " << str << endl;
+    transform(str.begin(),str.end(), back_inserter(res), op);
+    cout << "After process: " << res << endl;
     return 0;
 }
