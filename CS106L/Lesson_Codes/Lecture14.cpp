1040b17f8731d6a739f5297540bac73b0d37cffd
warrior <1986411713@qq.com>
2024-05-20 15:46:43 +0800

    完成Lecture14

--- CS106L/Lesson_Codes/Lecture14.cpp
+++ CS106L/Lesson_Codes/Lecture14.cpp
@@ -3,33 +3,40 @@
 // namespace
 
 #include <iostream>
+#include <string>
 
-/*namespace Lecture14{
+namespace Lecture14{
     class person{
     public:
         std::string name;
         void printName() const;
+        person(const std::initializer_list<char>& str);
     };
-}*/
+}
 
 
 
 int main(int argc, const char* argv[]){
-
+    Lecture14::person p{'a','b','c', 'd'};   //这里使用了initializer_list来进行初始化
+    p.printName();
+    //std::string str("abc");    这里并不是使用的统一列表初始化方式来初始化的，{}并不能作为判断是否使用了统一列表初始化的依据
+    //std::cout << str;
     return 0;
 }
 
-/* 使用namespace包装函数（类）时，用namespace{}分别包装函数（类）的声明和定义即可
- *
- * namespace Lecture14{
+ //使用namespace包装函数（类）时，用namespace{}分别包装函数（类）的声明和定义即可
+ namespace Lecture14{
     void person::printName() const{
         std::cout << name << std::endl;
     }
-}*/
+    person::person(const std::initializer_list<char>& str):name(str){}
+}
 
 
 /*
- * 通过定义一个仅包含纯虚函数的类可以实现类似于java中的interface的功能
- * 但是关于public,private继承方式的不同，导致的对类中成员的访问权限有何不同，需要进一步学习c++ primer plus
+ * 一个仅包含纯虚函数的类可以实现类似于java中的interface的功能
+ * 纯虚函数声明语法：virtual return_type funcname() = 0
+ * 虚函数声明语法：前面加virtual关键字即可
+ * 包含纯虚函数的类不能通过实例化创建对象
  * */
 
