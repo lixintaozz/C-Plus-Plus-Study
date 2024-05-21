79b377d545cfdf931343af9f71e4b38027ea606f
warrior <1986411713@qq.com>
2024-05-21 19:41:23 +0800

    添加Lecture15.cpp

--- CS106L/Lesson_Codes/Lecture14.cpp
+++ CS106L/Lesson_Codes/Lecture14.cpp
@@ -35,7 +35,7 @@ int main(int argc, const char* argv[]){
 
 /*
  * 一个仅包含纯虚函数的类可以实现类似于java中的interface的功能
- * 纯虚函数声明语法：virtual return_type funcname() = 0
+ * 纯虚函数声明语法：virtual return_type funcname() = 0;
  * 虚函数声明语法：前面加virtual关键字即可
  * 包含纯虚函数的类不能通过实例化创建对象
  * */
---
+++ CS106L/Lesson_Codes/Lecture15.cpp
@@ -0,0 +1,10 @@
+//
+// Created by warrior on 2024/5/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char* argv[]){
+
+    return 0;
+}
