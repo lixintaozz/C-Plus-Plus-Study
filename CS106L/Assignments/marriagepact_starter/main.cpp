96cbaffcce218fb04e9e18c690ac6ed4978de5c7
warrior <1986411713@qq.com>
2024-05-25 17:03:56 +0800

    添加assignment2 marriagepact

---
+++ CS106L/Assignments/marriagepact_starter/main.cpp
@@ -0,0 +1,33 @@
+/* 
+Assignment 2: Marriage Pact
+
+This assignment asks you to discover your one true love using containers and pointers.
+There are two parts: implement get_applicants and find_matches.
+
+Submit to Paperless by 11:59pm on 2/1/2024.
+*/
+
+#include <iostream>
+#include <string>
+#include <queue>
+#include <set>
+
+
+
+std::set<std::string> get_applicants(std::string filename) {
+
+    std::ifstream ifs();
+
+}
+
+std::queue<std::string*> find_matches(std::set<std::string> &students) {
+    // TODO: Implement this function.
+}
+
+
+
+int main() {
+    // Your code goes here. Don't forget to print your true love!
+    
+    return 0;
+}
