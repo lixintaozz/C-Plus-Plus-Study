b4d6693e85fe7f81ff88491ec9a828eaa8a64f15
warrior <1986411713@qq.com>
2024-05-25 18:20:53 +0800

    完成assignment2 marriage_pact

--- CS106L/Assignments/marriagepact_starter/main.cpp
+++ CS106L/Assignments/marriagepact_starter/main.cpp
@@ -11,23 +11,54 @@ Submit to Paperless by 11:59pm on 2/1/2024.
 #include <string>
 #include <queue>
 #include <set>
+#include <fstream>
+#include <vector>
 
+const std::string path = R"(D:\CPlusPlus\CS106L\Assignments\marriagepact_starter\students.txt)";
 
+std::set<std::string> get_applicants(const std::string& filename) {
 
-std::set<std::string> get_applicants(std::string filename) {
-
-    std::ifstream ifs();
-
+    std::ifstream ifs(filename);
+    //使用is_open()方法来检查文件是否打开
+    if (!ifs.is_open()){
+        std::cout << "Oops, File can't open!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    std::string line;
+    std::set<std::string> sets;
+    while (std::getline(ifs, line)){
+        sets.insert(line);
+        //std::cout << line << std::endl;
+    }
+    return sets;
 }
 
 std::queue<std::string*> find_matches(std::set<std::string> &students) {
-    // TODO: Implement this function.
+    //假设名字叫Stephen Curry
+    std::queue<std::string *> queue;
+    for (auto &item: students) {
+        size_t pos = item.find(" ") + 1;
+        bool flag = item[0] == 'S' && item[pos] == 'C';
+        if (flag) {
+            // 在C++中，std::set 内部存储的元素默认为const
+            // 在编译器层面，这意味着如果您尝试获取 std::set 中一个元素的指针，
+            // 您将得到一个指向 const 数据的指针（即 const std::string*）
+            // 因此在这里需要使用const_cast<std::string*>()将其转换为std::string*
+            auto p = const_cast<std::string *>(&item);
+            queue.push(p);
+        }
+    }
+    return queue;
 }
 
 
 
-int main() {
-    // Your code goes here. Don't forget to print your true love!
-    
+int main(){
+    auto set = get_applicants(path);
+    auto queue = find_matches(set);
+    while(!queue.empty()){
+        std::cout << *queue.front()  << std::endl;
+        queue.pop();
+    }
     return 0;
 }
